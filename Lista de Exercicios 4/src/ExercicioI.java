4420d8ae96494b03bede8ba617f5dda342d7d1d4
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-04-15 20:05:06 -0300

    exercício 1 - digitos no vetor

--- Lista de Exercicios 4/src/ExercicioI.java
+++ Lista de Exercicios 4/src/ExercicioI.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class ExercicioI {
+    public static void main(String[] args) {
+        decompor(1300);
+    }
+
+    public static void decompor(long numero){
+        int i, TL = 0; long aux = numero;
+
+        for(i = 0; aux > 0; i++){ // quantidade de dígitos
+            aux /= 10;
+            TL++;
+        }
+
+        long[] vetor = new long[TL]; // atribui um tamanho para o vetor (nº de digitos)
+
+        for(i = TL - 1; i >= 0; i--){ // passa os valores de tras pra frente no vetor
+            vetor[i] = numero % 10;
+            numero /= 10;
+        }
+
+        for(long e:vetor)
+            System.out.print(e+" ");
+    }
 }
