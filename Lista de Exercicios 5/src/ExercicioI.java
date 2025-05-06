57e7d1c385f44541e60992a9932f4478fb5f0f34
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2025-05-06 20:29:45 -0300

    exercicios

--- Lista de Exercicios 5/src/ExercicioI.java
+++ Lista de Exercicios 5/src/ExercicioI.java
@@ -1,7 +1,32 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.Scanner;
+
 public class ExercicioI {
     public static void main(String[] args) {
         // qual pasta quer analisar
         // que palavra você quer encontrar no arquivo
         // listar todos os arquivos, se tiver a extensão, abrir o arquivo e ler linha por linha
+
+
+        JFileChooser jFileChooser = new JFileChooser("c:\\");
+        jFileChooser.setDialogTitle("Escolha uma pasta:");
+        jFileChooser.showOpenDialog(null);
+        String pasta = jFileChooser.getCurrentDirectory().getAbsolutePath();
+        File folder = new File(pasta);
+        if(folder.isDirectory()){
+            File[] arquivos = folder.listFiles();
+            System.out.println("Arquivos da pasta selecionada:");
+            for(File file : arquivos)
+                System.out.println(file.getName());
+
+            Scanner lerPalavra = new Scanner(System.in);
+            System.out.print("Que palavra você quer encontrar no arquivo?: ");
+            String palavra = lerPalavra.next();
+            System.out.println("Ok, você deseja encontrar arquivos que tenham a palavra "+"'"+palavra+"'.");
+
+                       
+
+        }
     }
 }
